50a484b2367567308ca96a41802526df5a07ef82
iZeaoGamer <jameshayward14@icloud.com>
2017-06-27 13:39:05 +0100

    Update and rename GameManager.php to OITC.php

--- src/Ad5001/GameManager/OITC.php
+++ src/Ad5001/GameManager/OITC.php
@@ -3,7 +3,7 @@
 use Ad5001\GameManager\Game;
 use pocketmine\Player;
 
-class <YOUR_GAME_NAME> extends Game {
+class <OITC> extends Game {
 
 public function onGameStart() { // When the game start (enought players)
 // $this->getServer()->broadcastMessage("Game started on {$this->getLevel()->getName()}");
